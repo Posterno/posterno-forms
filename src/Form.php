124ad70059a8ab86acc64d7df60c01fa34e9f878
Alessandro Tesoro <alessandrotesoro@users.noreply.github.com>
2019-04-26 13:54:19 +0100

    added exception class

--- src/Form.php
+++ src/Form.php
@@ -3,7 +3,7 @@
  * Forms generator class for Posterno.
  *
  * @package     posterno
- * @copyright   Copyright (c) 2018, Pressmodo, LLC
+ * @copyright   Copyright (c) 2009-2019 NOLA Interactive, LLC, Copyright (c) 2019, Sematico, LTD.
  * @license     http://opensource.org/licenses/gpl-2.0.php GNU Public License
  * @since       0.1.0
  */
