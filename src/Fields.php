3cc2a40a05b2762a95641868f1868776502dab9b
Alessandro Tesoro <alessandrotesoro@users.noreply.github.com>
2019-05-10 11:47:45 +0100

    load opening hours field

--- src/Fields.php
+++ src/Fields.php
@@ -103,6 +103,9 @@ class Fields {
 			case 'listing-location':
 				$element = new Element\ListingLocation( $name, $value, $indent );
 				break;
+			case 'listing-opening-hours':
+				$element = new Element\ListingOpeningHours( $name, $value, $indent );
+				break;
 			default:
 				$name  = $multiple === true ? $name . '[]' : $name;
 				$class = 'PNO\\Form\\Element\\Input\\' . ucfirst( strtolower( $type ) );
